60af47dc1af1c7779db79d434e9dca1f8fd6c65b
Kilosh254 <Me.kilonzo@gmail.com>
2022-07-04 03:51:26 -0700

    C hello world Task 4_3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,6 @@
 #include <studio.h>
-int main(){
-	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
