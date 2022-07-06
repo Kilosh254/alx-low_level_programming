1aa250235c2454e7be126081fed2229fcaa35aca
Kilosh254 <Me.kilonzo@gmail.com>
2022-07-06 01:39:13 -0700

    C Functions Task 0_2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,10 +3,12 @@
 /**
  * print_alphabet -> prints the lowercase alphabets
  */
+
 void print_alphabet(void)
 {
 	int j;
-	for (j= 'a'; j <= 'z'; j++)
+	for (j = 'a'; j <= 'z'; j++)
+
 	{
 		_putchar(j);
 	}
