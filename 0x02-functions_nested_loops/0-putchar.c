bd6efedfde7df9fc0c3c9ff6fd8dbb91807c1d6e
Kilosh254 <Me.kilonzo@gmail.com>
2022-07-05 23:10:45 -0700

    C Functions Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,19 @@
-#include "main.h"
+#include "main.h";
 /**
- * main - Entry point
+ * main - Prints putchar
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 int main(void)
 {
-char joy[9] = "_putchar";
-int i;
-for (i = 0; i < 8; i = i + 1)
-{
-_putchar(joy[i]);
-}
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 return (0);
 }
