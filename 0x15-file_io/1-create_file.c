ef81975e277004f454d64034e809c57c9222bd08
Kilosh254 <Me.kilonzo@gmail.com>
2023-01-21 00:31:13 -0800

    Task 1_2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,20 +9,16 @@
 
 int create_file(const char *filename, char *text_content)
 {
-		int fd;
-
-			if (!filename)
-						return (-1);
-
-				fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-					if (fd == -1)
-								return (-1);
-
-						if (text_content)
-									write(fd, text_content, _strlen(text_content));
-
-							close(fd);
-								return (1);
+int fd;
+if (!filename)
+return (-1);
+fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+if (fd == -1)
+return (-1);
+if (text_content)
+write(fd, text_content, _strlen(text_content));
+close(fd);
+return (1);
 }
 
 /**
@@ -33,10 +29,8 @@ int create_file(const char *filename, char *text_content)
 
 int _strlen(char *s)
 {
-		int c = 0;
-
-			while (s[c])
-						c++;
-
-				return (c);
+int c = 0;
+while (s[c])
+c++;
+return (c);
 }
