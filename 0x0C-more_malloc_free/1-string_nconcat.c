3f7456e0c16aac96bdc5d4c9a40f1d584a6bf7c6
Kilosh254 <Me.kilonzo@gmail.com>
2023-01-23 01:53:54 -0800

    Task 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - array
+ * @s1: pointers
+ * @s2: pointers
+ * @n: Variable
+ * Return: value
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+unsigned int i, x, j, e, d, m;
+char *a;
+if (s1 == NULL)
+{
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
+for (i = 0; s1[i] != '\0'; i++)
+{
+for (x = 0; s2[x] != '\0'; x++)
+{
+if (x < n)
+d++;
+}
+a = malloc((i + n + 1) * sizeof(char));
+if (a == NULL)
+{
+return (NULL);
+}
+for (j = 0; j < i; j++)
+{
+a[j] = s1[j];
+}
+m = 0;
+for (e = 0; e < n; e++)
+{
+a[e + j] = s2[m];
+m++;
+}
+a[e + j] = '\0';
+return (a);
+}
